6e356a2b5f2a863d6a15b568e38717b66716919a
Trakatranks <a.ferlet@student.isartdigital.com>
2017-04-14 07:19:40 +0200

    commit menus

---
+++ Assets/LevelRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelRotation : MonoBehaviour {
+    Transform tfm;
+    public float coeff = 1f;
+
+    // Use this for initialization
+    void Start () {
+        tfm = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tfm.Rotate(new Vector3(0, coeff, 0));
+	}
+}
